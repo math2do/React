3667dd93b6215e6c4d9ac1d5e32340a3cda61635
math2do <mathura.dakshana15@gmail.com>
2023-11-26 12:28:52 +0530

    added project birthday

--- src/projects/birthday/Birthday.js
+++ src/projects/birthday/Birthday.js
@@ -1,8 +1,41 @@
 import "./style.css"
+import { useState } from "react"
+import data from "./data"
 
 const App = () => {
+  const [birthdays, setBirthdays] = useState(data)
+
+  const clearAll = () => {
+    setBirthdays([])
+  }
+
   return (
-    <h3>Hello from birthday boy</h3>
+    <main className="container">
+      <div className="section">
+        <h1>{birthdays.length} Birthdays Today</h1>
+        {
+          birthdays.map((birthday) => {
+            return <Birthday key={birthday.id} {...birthday} />
+          })
+        }
+
+        <button onClick={() => { clearAll() }}>clear all</button>
+      </div>
+    </main>
+  );
+}
+
+function Birthday(props) {
+  const { name, age, image } = props;
+
+  return (
+    <div className='person'>
+      <img src={image} alt={image} />
+      <div className="desc">
+        <h2 className='name'>{name}</h2>
+        <h4 className='age'>{age} years</h4>
+      </div>
+    </div>
   );
 }
 
