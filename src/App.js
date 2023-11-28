746d2514c64701d78874c4c8e0d8e3297fb4797a
math2do <mathura.dakshana15@gmail.com>
2023-11-29 00:57:54 +0530

    add project reviews

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Tours from './projects/tours/Tours'
+import Reviews from './projects/reviews/Reviews'
 
 // App is like a main function, just wire any component to load
 function App() {
   return (
-    <Tours />
+    <Reviews />
   );
 }
 
---
+++ src/projects/reviews/Reviews.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import data from "./data";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import "./style.css";
+
+const Reviews = () => {
+  const [index, setIndex] = useState(0);
+  const next = () => {
+    let nextIndex = index + 1;
+    if (nextIndex == data.length) {
+      nextIndex = 0;
+    }
+    setIndex(nextIndex);
+  }
+
+  const prev = () => {
+    let prevIndex = index - 1;
+    if (prevIndex === -1) {
+      prevIndex = data.length - 1;
+    }
+    setIndex(prevIndex);
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const surprise = () => {
+    const newIndex = getRandomInt(data.length);
+    setIndex(newIndex);
+  }
+
+  return (
+    <main className="container">
+      <Review {...data[index]}
+        surprise={surprise} prev={prev} next={next} />
+    </main>
+  );
+}
+
+function Review({ id, name, job, image, text, surprise, prev, next }) {
+  return (
+    <div className="review">
+      <div className="image">
+        <img src={image} alt={name} />
+        <div className="quote">
+          <FontAwesomeIcon icon={faQuoteRight} />
+        </div>
+
+      </div>
+      <h1>{name}</h1>
+      <h4>{job}</h4>
+      <p>{text}</p>
+
+      <div className="arrows">
+
+        <FontAwesomeIcon className='arrow_left' icon={faArrowLeft} onClick={() => { prev() }} />
+        <FontAwesomeIcon className='arrow_right' icon={faArrowRight} onClick={() => { next() }} />
+
+      </div>
+
+      <button onClick={() => { surprise() }}>Surprise Me</button>
+    </div>
+  );
+}
+
+export default Reviews;
